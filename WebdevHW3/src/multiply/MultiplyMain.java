d5ba47e357ed666b536dcd4b21fa38115a577746
Sarick Shah <shahsarick@gmail.com>
2016-02-17 18:03:46 -0500

    GithubNewSetup

---
+++ WebdevHW3/src/multiply/MultiplyMain.java
@@ -0,0 +1,25 @@
+package multiply;
+import java.util.Scanner; 
+public class MultiplyMain {
+	static int c = 0;
+	static int a= 0; 
+	static int b=0;
+
+	public static class MultiplyMethod{
+		public int getMult(){
+			Scanner input = new Scanner(System.in); 
+			System.out.println("Enter in the first integer to multiply: "); 
+			a = input.nextInt(); 	
+			System.out.println("Enter in the second integer to multiply: ");
+			b = input.nextInt(); 	
+			c = a*b;
+			return c;
+				
+		}
+	public static void main(String args[]) {
+			MultiplyMethod m = new MultiplyMethod();
+			m.getMult();
+			System.out.println("The product is " + c);
+		}
+	}
+}
